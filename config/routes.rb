07c95ee4571c02cf547acbea0bcc1f1eb5abfa24
yamakamineyu <yamamoto-luffysiro@docomo.ne.jp>
2021-07-05 18:31:16 +0900

    rails g controller users
    ログイン機能のためのcontrollerを作成。

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   get '/users/sign_up', to: 'devise/registrations#new'
 
 
-  # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'top#index'
 end
