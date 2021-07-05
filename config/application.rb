6938e86f4c12fab95da345387dbd670d7c044002
yamakamineyu <yamamoto-luffysiro@docomo.ne.jp>
2021-07-05 16:08:31 +0900

    rails g devise user / rails db:create
    rails g devise user
    （他のサイトの参考と被り、こんがらがり再度作成）
    rails db:create
    既に作成済み（deleteせずに残っている状態）

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+# require Rails.root.join(“app/models/user.rb”)
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'sessions/new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
 
-  devise_for :users
+  get '/users/sign_up', to: 'devise/registrations#new'
+
+
+  # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'top#index'
 end
